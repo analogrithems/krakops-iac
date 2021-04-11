98a12446babc629a3a2514ee065c6e94d22c19cd
Aaron Collins <Aaron.Collinsa@gmail.com>
2021-04-11 01:53:56 -1000

    cleanup

--- projects/infra/ecr_repo.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "this" {
-  name                 = "litecoin-node"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
--- projects/infra/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  cluster_name = "${var.vpc_name}-krakops-${random_string.suffix.result}"
-
-  common_tags = {
-    project   = "infrastructure"
-    terraform = "true"
-    vpc_name  = var.vpc_name
-  }
-}
